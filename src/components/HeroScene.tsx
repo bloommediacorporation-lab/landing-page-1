834ea8f6ed64e83bf6b8383f2530fe1f84c849a0
Bloom Media Corporation <bloommediacoporation@gmail.com>
2026-04-03 14:42:44 +0300

    fix: add missing Suspense import

--- src/components/HeroScene.tsx
+++ src/components/HeroScene.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import React, { useRef, useEffect, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Float, Environment } from "@react-three/drei";
 import * as THREE from "three";
